abf30c381a22170e0d899da5c468d80d67074f35
KAMDEM TCHIBONSOU Dhalil <Dhalil.KAMDEM-TCHIBONSOU@saipem.com>
2025-08-06 10:51:56 +0200

    first commit

---
+++ backend/testDjango/src/api/serializers/__init__.py
@@ -0,0 +1,1 @@
+from .itemSerializer import ItemSerializer
---
+++ backend/testDjango/src/apitestdjango/settings/prod.py
@@ -0,0 +1,29 @@
+
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+from .base import *
+load_dotenv()
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.getenv('SECRET_KEY')
+if not SECRET_KEY:
+    raise ValueError("SECRET_KEY must be set in your .env file.")
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG =  os.getenv("DEBUG", "True").lower() in ("true", "1")
+print("DEBUG =", DEBUG)
+
+
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "")
+if not ALLOWED_HOSTS:
+    raise ValueError("ALLOWED_HOSTS must be set in your .env file.")
+ALLOWED_HOSTS = [host.strip() for host in ALLOWED_HOSTS.split(",") if host.strip()]
+print("ALLOWED_HOSTS =", ALLOWED_HOSTS)
